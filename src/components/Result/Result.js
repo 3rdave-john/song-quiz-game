9979a7e14fe4f15327e69d99935a71d6b6a9ab43
John Yu <john@3rdave.com>
2018-03-20 04:47:06 -0700

    setResults changed, added  switch function Results.js

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React from 'react';
 import './Result.css';
 
-function Result(props) {
-  return (
-    <div className="result">
-      You prefer <strong>{props.quizResult}</strong>!
-    </div>
-  );
-}
 
+
+function Result(props) {
+  switch (props.quizResult) {
+    case 'bold':
+      return <div><h1>bold</h1></div>;
+    case 'dreamy':
+      return <div><h1>dreamy</h1></div>;
+    case 'flirty':
+      return <div><h1>flirty</h1></div>;
+    case 'guilty':
+      return <div><h1>guilty</h1></div>;
+    case 'jaded':
+      return <div><h1>jaded</h1></div>;
+  }
+}
+// function Result(props) {
+
+//   return (
+//     <div className="result">
+//       You prefer <strong>{props.quizResult}</strong>!
+//     </div>
+//   );
+// }
 // Result.propTypes = {
 //   quizResult: React.PropTypes.string.isRequired,
 // };
